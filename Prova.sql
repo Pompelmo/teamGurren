10c2782d9e3c504cf1055ede4f6f7fdcc309a549
Pompelmo <gallo.kate@gmail.com>
2015-06-01 12:11:56 +0200

    Prova
    Niente di che, questo può anche essere cancellato perché non si può
    usare il modulo

--- Prova.sql
+++ Prova.sql
@@ -1,19 +1,34 @@
--- file di prova
 /*
-	http://www.postgresql.org/docs/9.2/static/tablefunc.html
-*/
+FILE DI PROVA
+
+http://www.postgresql.org/docs/9.2/static/tablefunc.html
+
 CREATE EXTENSION tablefunc;
+*/
 
 CREATE TABLE B_opens
 (
-	business_id char(22) PRIMARY KEY,
-	Monday time DEFAULT NULL,
-	Tuesday time DEFAULT NULL,
-	Wednesday time DEFAULT NULL,
-	Thursday time DEFAULT NULL,
-	Friday time DEFAULT NULL,
-	Saturday time DEFAULT NULL,
-	Sunday time DEFAULT NULL
+	business_id char(22),
+	Monday time,
+	Tuesday time,
+	Wednesday time,
+	Thursday time,
+	Friday time,
+	Saturday time,
+	Sunday time
+	)
+;
+
+CREATE TABLE B_closes
+(
+	business_id char(22),
+	Monday time,
+	Tuesday time,
+	Wednesday time,
+	Thursday time,
+	Friday time,
+	Saturday time,
+	Sunday time
 	)
 ;
 
@@ -32,14 +47,24 @@ CREATE temporary TABLE t (
 	)
 ;
 
+CREATE temporary TABLE weekday
+(
+	day varchar(9)
+	)
+;
+	
+INSERT INTO weekday(day) VALUES('Monday'), ('Tuesday'), 
+('Wednesday'), ('Thursday'), ('Friday'), ('Saturday'), ('Sunday');
+
 COPY t
 FROM '/Users/Kate/Desktop/SECONDO_SEMESTRE/BASE_DI_DATI/PROGETTO/business-openhours.csv' 
-DELIMITER ',' CSV HEADER;
+DELIMITER ',' CSV HEADER
+;
 
-
+INSERT INTO B_opens
 SELECT * 
 FROM crosstab('SELECT business_id, day, opens FROM t', 
-			  'SELECT m FROM generate_series(1,7) m') 
+			  'SELECT day FROM weekday') 
 			AS (business_id char(22), 
 				 "Monday" time, 
 				 "Tuesday" time,
@@ -48,23 +73,26 @@ FROM crosstab('SELECT business_id, day, opens FROM t',
 				 "Friday" time, 
 				 "Saturday" time, 
 				 "Sunday" time) 
-LIMIT 5
-;								
+ORDER BY business_id				
+;
 
+INSERT INTO B_closes
+SELECT * 
+FROM crosstab('SELECT business_id, day, closes FROM t', 
+			  'SELECT day FROM weekday') 
+			AS (business_id char(22), 
+				 "Monday" time, 
+				 "Tuesday" time,
+				 "Wednesday" time, 
+				 "Thursday" time,
+				 "Friday" time, 
+				 "Saturday" time, 
+				 "Sunday" time) 
+ORDER BY business_id				
+;	
+
+DROP TABLE weekday;
 DROP TABLE t;
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 

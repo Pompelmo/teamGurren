871f6aeeab1f328e30c065e3e88ed30ee897c81a
zaifoski <sofiachimirri@gmail.com>
2015-06-09 16:31:06 +0200

    aggiunto il sixth part
    secondo me dovrebbe funzionare salvo anche qui il problema con la codifica...

--- provaquery00.sql
+++ provaquery00.sql
@@ -87,7 +87,23 @@ LIMIT 10
 -- ##################################### --
 -- Sixth part, recreate user-friends.csv --
 -- ##################################### --
+-- ##################################### --
+-- Sixth part, recreate user-friends.csv --
+-- ##################################### --
 
+COPY
+(SELECT *
+FROM	(SELECT
+		record_type.user_friends_type AS record_type,
+     		U_friends.user_id,U_info.name,U_friends.friend_id
+	FROM U_friends,U_info,record_type
+	WHERE U_friends.user_id=U_info.user_id
+	) AS friends_table
+ORDER BY user_id)
+TO '/tmp/user-friends.csv'
+CSV HEADER
+
+;
 -- ########################################### --
 -- Seventh part, recreate user-compliments.csv --
 -- ########################################### --

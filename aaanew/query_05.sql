076fc2749b16e97aac5e526a03b519164e87377e
zaifoski <sofiachimirri@gmail.com>
2015-06-10 19:41:42 +0200

    aggiunto se open = true

--- aaanew/query_05.sql
+++ aaanew/query_05.sql
@@ -1,10 +1,13 @@
-
-SELECT a.city AS "city", 
-		c.neighborhood, AS "neighborhood"
-  		max(latitude) AS "max_lat", 
-		max(longitude) AS "max_long",
-		min(latitude) AS "min_lat",
-		min(longitude) AS "min_long"
+SELECT
+	a.city,
+	c.neighborhood,
+	max(latitude)	AS "max lat",
+	max(longitude) 	AS "max long",
+	min(latitude) 	AS "min lat",
+	min(longitude) 	AS "min long"
 FROM B_address a JOIN B_coord c ON (a.business_id = c.business_id)
+WHERE a.open = true
 GROUP BY city, neighborhood
 ;
+--2,372 ms
+--69 rows

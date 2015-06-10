fc097c298361325ab537bbb0a980e7e6c5c3f003
Pompelmo <gallo.kate@gmail.com>
2015-06-10 17:36:39 +0200

    join nel from e gli AS diversi

--- aaanew/query_05.sql
+++ aaanew/query_05.sql
@@ -1,5 +1,10 @@
-SELECT a.city,c.neighborhood,max(latitude) AS max_lat,max(longitude) AS max_long,min(latitude) AS min_lat,min(longitude) AS min_long
-FROM B_address a, B_coord c
-WHERE a.business_id = c.business_id
+
+SELECT a.city AS "city", 
+		c.neighborhood, AS "neighborhood"
+  		max(latitude) AS "max_lat", 
+		max(longitude) AS "max_long",
+		min(latitude) AS "min_lat",
+		min(longitude) AS "min_long"
+FROM B_address a JOIN B_coord c ON (a.business_id = c.business_id)
 GROUP BY city, neighborhood
 ;

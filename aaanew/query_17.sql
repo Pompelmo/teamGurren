1618927d2e2391cbd9d39e3e47b4f72e60e7c5df
zaifoski <sofiachimirri@gmail.com>
2015-06-10 19:47:37 +0200

    droppato indice più aggiunto spazietto

--- aaanew/query_17.sql
+++ aaanew/query_17.sql
@@ -1,10 +1,11 @@
 CREATE INDEX hash_friends ON U_friends USING HASH (user_id);
-SELECT t1.user_id, t2.friend_id AS suggested_user_id
+SELECT t1.user_id, t2.friend_id AS "suggested user_id"
 FROM U_friends t1 JOIN U_friends t2 ON (t1.friend_id = t2.user_id)
 WHERE t2.friend_id NOT IN
        (SELECT t3.friend_id
        FROM U_friends t3
        WHERE t3.user_id = t1.user_id)
 ;
+DROP INDEX hash_friends;
 --25793 rows
---3545,045 ms
+--3545,045 ms (+ 73,350ms per create index + 22,265 per drop index)

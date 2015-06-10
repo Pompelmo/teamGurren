151fada9180040eb5d3c1c1c78fdc41cbcd8d6e8
zaifoski <sofiachimirri@gmail.com>
2015-06-10 19:34:17 +0200

    nuova versione con hash index

--- aaanew/query_17.sql
+++ aaanew/query_17.sql
@@ -1,8 +1,11 @@
+CREATE INDEX hash_friends ON U_friends USING HASH (user_id);
+with tabella as (
 SELECT t1.user_id, t2.friend_id AS suggested_user_id
-FROM U_friends t1,U_friends t2
-WHERE t1.friend_id = t2.user_id
-AND t2.friend_id NOT IN
+FROM U_friends t1 JOIN U_friends t2 ON (t1.friend_id = t2.user_id)
+WHERE t2.friend_id NOT IN
        (SELECT t3.friend_id
        FROM U_friends t3
        WHERE t3.user_id = t1.user_id)
-;
+)
+--25793 rows
+--3545,045 ms

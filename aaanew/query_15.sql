714f541220e7c8b22b3d1a82d5bac08aac9bae8d
zaifoski <sofiachimirri@gmail.com>
2015-06-10 22:06:44 +0200

    Update query_15.sql

--- aaanew/query_15.sql
+++ aaanew/query_15.sql
@@ -1,22 +1,24 @@
--- c1 --> business_id delle 10% meno recensite
 -- c2 --> per ogni user_id che ha recensito delle c1, numero di recensioni totali
 -- c3 --> per ogni user_id che ha recensito delle c1, numero di recensioni c1
---with tabella as(
-WITH	c1 AS (SELECT business_id
-	      FROM B_address
-	      ORDER BY review_count ASC
-	      LIMIT (SELECT COUNT(*) AS INTEGER FROM B_address)),
+with tabella as(
+WITH	
 	c2 AS (SELECT DISTINCT R.user_id, COUNT(R.business_id)
 	      FROM R_stars R
 	      GROUP BY R.user_id),
 	c3 AS (SELECT DISTINCT R.user_id, COUNT(R.business_id)
 	      FROM R_stars R
-	      WHERE R.business_id IN (SELECT business_id FROM c1)
+	      WHERE R.business_id IN
+	      (
+		SELECT business_id
+	      	FROM B_address
+	      	ORDER BY review_count ASC
+	      	LIMIT (SELECT (COUNT(*)/10) AS INTEGER FROM B_address)
+	      )
 	      GROUP BY R.user_id)
 SELECT DISTINCT c2.user_id
 FROM c2,c3
 WHERE c3.count = 0.75*c2.count
---)select count (*) from tabella
+)select count (*) from tabella
 ;
---1071,835 ms
---395 rows
+--808,001 ms
+--297 rows

e7f6841f1efe543bf0c4645d6100e9080e9cc71d
Pompelmo <gallo.kate@gmail.com>
2015-06-10 22:22:24 +0200

    teniamo except?

--- aaanew/query_17.sql
+++ aaanew/query_17.sql
@@ -1,28 +1,14 @@
+/*
+		QUERY 17 - Friends Finder
+*/
 
---CREATE INDEX hash_friends ON U_friends USING HASH (user_id);
-/*WITH AA AS(SELECT t1.user_id, t2.friend_id AS "suggested user_id"
-		FROM U_friends t1 JOIN U_friends t2 ON (t1.friend_id = t2.user_id)
-		WHERE t2.friend_id NOT IN
-       (SELECT t3.friend_id
-       FROM U_friends t3
-       WHERE t3.user_id = t1.user_id)
+SELECT user_id, friend_id AS suggested_user_id
+FROM (
+	(SELECT distinct t1.user_id, t2.friend_id
+	FROM u_friends t1 JOIN u_friends t2 ON (t1.friend_id = t2.user_id) 
 	)
-SELECT COUNT(*)
-FROM (SELECT DISTINCT * FROM AA) A */	
+		EXCEPT
+	(SELECT *
+	FROM u_friends) 
+	) A
 ;
---DROP INDEX hash_friends;
---25793 rows
---3545,045 ms (+ 73,350ms per create index + 22,265 per drop index)
-
-
-
-(SELECT distinct t1.user_id, t2.friend_id
-FROM u_friends t1 JOIN u_friends t2 ON (t1.friend_id = t2.user_id))
-	EXCEPT
-(SELECT *
-FROM u_friends	)
-;
-
---  25188 rows
---  933,029 ms
-
